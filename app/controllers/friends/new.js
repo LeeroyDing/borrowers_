64bf7b87ea9f6e0e703eb2ba42493b9198a09d9a
Leeroy Ding <i@leeroy.im>
2015-12-21 23:31:58 +0000

    Add some validators

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName',
+    'model.twitter',
+    {
+      get() {
+        return
+        !Ember.isEmpty(this.get('model.email')) &&
+        !Ember.isEmpty(this.get('model.firstName')) &&
+        !Ember.isEmpty(this.get('model.lastName')) &&
+        !Ember.isEmpty(this.get('model.twitter'));
+      }
+    }),
   actions: {
     save() {
       console.log('+- save action in friends new controller');
