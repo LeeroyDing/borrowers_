2e7c461a0c109b272fb479b847d31452154062da
Leeroy Ding <i@leeroy.im>
2015-12-23 12:57:51 +0000

    Add property fullName to model friends

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
@@ -6,5 +7,10 @@ export default DS.Model.extend({
   email: DS.attr('string'),
   twitter: DS.attr('string'),
   totalArticles: DS.attr(),
-  number: DS.attr()
+  fullName: Ember.computed('firstName', 'lastName',
+  {
+    get() {
+      return this.get('firstName') + ' ' + this.get('lastName');
+    }
+  })
 });
