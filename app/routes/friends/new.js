0151ade4b2bbcee051fc4f8d2d971989803607c9
Leeroy Ding <i@leeroy.im>
2016-01-07 21:16:07 +0000

    Discard unsaved record in friends/new

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,6 +4,14 @@ export default Ember.Route.extend({
   model() {
     return this.store.createRecord('friend');
   },
+  resetController: function (controller, isExiting, transition) {
+    if (isExiting) {
+      var model = controller.get('model');
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+    }
+  },
   actions: {
     save() {
       console.log('+-- save action bubbled up to friends new route');
