d017ac52d7574ec730cf897375b1744c3e20a57a
Leeroy Ding <i@leeroy.im>
2016-01-08 20:42:56 +0000

    Add moment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -36,5 +36,7 @@ module.exports = function(defaults) {
 
   app.import('bower_components/picnic/releases/plugins.min.css');
 
+  app.import('bower_components/moment/moment.js');
+
   return app.toTree();
 };
