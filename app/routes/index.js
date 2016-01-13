99534308e152d1f1d88fb7e5e934726d46aa3925
Leeroy Ding <i@leeroy.im>
2016-01-13 22:31:15 +0000

    Fix syntax

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
+import request from 'ic-ajax';
 
 export default Ember.Route.extend({
   model() {
-    return request('/api/friends').then(function(data) {
+    return request('http://localhost:4000/api/friends').then(function(data) {
       return {
         friendsCount: data.friends.length
-      }
-    })
+      };
+    });
   }
 });
